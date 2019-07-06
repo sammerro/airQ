0e20707642e4cb1819f2775e931f92f46db44a97
Michal <michal.kowalski.wro@gmail.com>
2019-07-06 21:10:52 +0200

    add vue.config.js for production

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: require("./aliases.config").webpack
+    }
+  },
+  css: {
+    // Enable CSS source maps.
+    sourceMap: true
+  },
+  baseUrl: process.env.NODE_ENV === "production" ? "/airq/" : "/"
+};
